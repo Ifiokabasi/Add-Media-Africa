56de8a019dac6c525576abd84ab1bae8bfe4ae64
Ifiokabasi Ettang <ieshawshank1@gmail.com>
2026-07-09 11:44:31 +0100

    updated site map

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/react-swc'; // or @vitejs/plugin-react
+import Sitemap from 'vite-plugin-sitemap';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://www.addmediaafrica.com',
+      dynamicRoutes: ['/about', '/service', '/contact', '/work'] // Add your subpages here
+    }),
+  ],
+});
