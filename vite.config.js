7a53d98a440296a48309ab31955138f05e36049d
Ifiokabasi Ettang <ieshawshank1@gmail.com>
2026-07-09 11:52:54 +0100

    updated site map working

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/react-swc'; // or @vitejs/plugin-react
+import react from '@vitejs/plugin-react'; // or @vitejs/plugin-react
 import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
